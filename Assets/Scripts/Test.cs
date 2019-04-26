6918dfd24c836fdaec7ce92458de142ad8790f90
SiiMeR <siimraudsepp@msn.com>
2019-04-26 20:55:01 +0300

    Add initial project files

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    private float test;
     // Start is called before the first frame update
     void Start()
     {
-        
+        test = 3;
     }
 
     // Update is called once per frame
