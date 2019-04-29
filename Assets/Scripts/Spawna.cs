7b5bf8f44dc6c3cb0b8a17078867bb280109ddb6
SiiMeR <siimraudsepp@msn.com>
2019-04-29 16:27:07 +0300

    Spawner and tress

---
+++ Assets/Scripts/Spawna.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawna : MonoBehaviour
+{
+
+    public GameObject prefab;
+
+    public int timer;
+
+    private float timespent;
+
+    public Transform point;
+    public GameObject currentPr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timespent += Time.deltaTime;
+
+        if (timespent > timer)
+        {
+            timespent = 0;
+            if (currentPr == null)
+            {
+               currentPr = Instantiate(prefab, point.position, Quaternion.identity);
+            }
+        }
+    }
+}
